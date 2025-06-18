d2f41df976f413c6744f806c6222c544570bf537
harrysap <nguyen03101998@gmail.com>
2025-06-18 10:44:49 +0700

    adjust route

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,27 @@
 "use client";
 
-import React, { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import React from "react";
+import styles from "./page.module.css";
+import Chat from "./components/chat";
+import FileViewer from "./components/file-viewer";
+import Header from "./components/header";
 
 const Home = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Redirect to /examples/all immediately
-    router.push("/examples/all");
-  }, [router]);
-
   return (
-    <main>
-      <div>Redirecting...</div>
+    <main className={styles.main}>
+      <div className={styles.wrapper}>
+        <Header />
+        <div className={styles.container}>
+          <div className={styles.column}>
+            <FileViewer />
+          </div>
+          <div className={styles.chatContainer}>
+            <div className={styles.chat}>
+              <Chat />
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
